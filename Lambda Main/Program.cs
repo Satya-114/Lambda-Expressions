c0698e33b22856623e95822fbd2671b09d0a7e3e
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-06-02 10:41:57 +0530

    Add files via upload

---
+++ Lambda Main/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Lambda_Main
+{
+    public delegate string MyDelegate(string name);
+    class Program
+    {
+        static void Main()
+        {
+            MyDelegate obj = ( name) =>
+            {
+                return "Hello" + name + "a very good morning";
+            };
+            string str = obj.Invoke("raju");
+            Console.WriteLine(str);
+        }
+    }
+}
